3fda390c68f965c1b55b31463ef6ae1c523bfb80
bca1456 <eugene.antipenko.s@gmail.com>
2019-10-28 12:21:53 +0300

    add sql exports

---
+++ src/main/java/com/example/juice/sql/books.sql
@@ -0,0 +1,3 @@
+INSERT INTO "books"("id", "name") VALUES (24, 'baba yaga');
+INSERT INTO "books"("id", "name") VALUES (25, 'kolobok');
+INSERT INTO "books"("id", "name") VALUES (26, 'harry potter');
---
+++ src/main/java/com/example/juice/sql/usr.sql
@@ -0,0 +1,1 @@
+INSERT INTO "usr"("id", "active", "password", "username") VALUES (27, 't', '1', 'u');
