8cd136c1024528f44abc3b506c3ce38cb2bd8add
bca1456 <eugene.antipenko.s@gmail.com>
2019-11-04 12:20:48 +0300

    fixed get books angular

--- src/main/java/com/example/juice/controller/BookController.java
+++ src/main/java/com/example/juice/controller/BookController.java
@@ -3,12 +3,13 @@ package com.example.juice.controller;
 import com.example.juice.domain.Book;
 import com.example.juice.repos.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
 @Controller
@@ -16,18 +17,26 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+//    @ExceptionHandler(Exception.class)
+//    public ResponseEntity<Object> handle(Exception ex,
+//                                         HttpServletRequest request, HttpServletResponse response) {
+//        if (ex instanceof NullPointerException) {
+//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+//        }
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//    }
 //    @GetMapping("/")
 //    public String greeting(Map<String, Object> model) {
 //        return "greeting";
 //    }
 
     @GetMapping("/books")
-    public List<Book> getBooks() {
+    public @ResponseBody List<Book> getBooks() {
         return (List<Book>) bookRepository.findAll();
     }
 
     @PostMapping("/books")
-    void addUser(@RequestBody Book book) {
+    public @ResponseBody void addUser(@RequestBody Book book) {
         bookRepository.save(book);
     }
 
